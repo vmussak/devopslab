939d76da96f82e923e37743deb9cca0c3273c382
Vinicius Mussak <vinicius.mussak@outlook.com>
2022-01-22 13:55:17 -0300

    Test Fail Pipeline

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ class Test(unittest.TestCase):
 
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
-        self.assertEqual(self.result.status_code, 200)
+        self.assertEqual(self.result.status_code, 500)
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World from Mussak App :)")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World from Mussak App :) 2")
